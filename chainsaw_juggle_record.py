ae764df51478045077011b4dfb437373903dd777
Stewart Christie <stewart.chrst99@gmail.com>
2022-09-21 11:23:24 -0500

    fixing conflicts with display all records

--- chainsaw_juggle_record.py
+++ chainsaw_juggle_record.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 """
 This progam uses a database that collects all chainsaw juggling records in 2018
@@ -5,25 +6,24 @@ The user is given the option to choose between:
     -displaying all recorded records in database 
     -add a new record to the database
     -modify and update an existing entry in the database
-    -Delete an entry in the 
+    -Delete an entry in the database
 """
 #establish a connection to chainjugglerec.db
 conn = sqlite3.connect('chainjugglerec.db')
 
-#create table for db
-conn.execute('CREATE TABLE IF NOT EXISTS records(name text, country text, number_of_catches integer)')
 
+
+#create table for db
+conn.execute('CREATE TABLE IF NOT EXISTS records(rowid INTEGER PRIMARY KEY, name text, country text, number_of_catches integer)')
 
 #add data to the table
-conn.execute('INSERT INTO records VALUES("Jane Mustonen", "Finland", 98)')
-conn.execute('INSERT INTO records VALUES("Ian Stewart", "Canada", 94)')
-conn.execute('INSERT INTO records VALUES("Aaron Gregg", "Canada", 88)')
-conn.execute('INSERT INTO records VALUES("Chad Taylor", "USA", 78)')
+conn.execute('INSERT INTO records VALUES(NULL, "Jane Mustonen", "Finland", 98)')
+conn.execute('INSERT INTO records VALUES(NULL, "Ian Stewart", "Canada", 94)')
+conn.execute('INSERT INTO records VALUES(NULL, "Aaron Gregg", "Canada", 88)')
+conn.execute('INSERT INTO records VALUES(NULL, "Chad Taylor", "USA", 78)')
 
 #finalize additions
 conn.commit()
-
-
 
 def main():
     menu_text = """
@@ -31,7 +31,7 @@ def main():
     2. Add new record
     3. Edit existing record
     4. Delete record 
-    6. Quit
+    5. Quit
     """
 
     while True:
@@ -46,7 +46,6 @@ def main():
         elif choice == '4':
             delete_record()
         elif choice == '5':
-            conn.close()
             break
         else:
             print('Not a valid selection, please try again')
